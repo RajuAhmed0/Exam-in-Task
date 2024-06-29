2ceedad1f7eb2477a7907df74b67f62ce5da315c
Your Name <you@example.com>
2024-06-29 09:01:18 +0600

    all task complate

---
+++ task.js
@@ -0,0 +1,52 @@
+// Task 1
+
+var MainMoney = 1000;
+var TwoKgFruitsPrice = 700;
+
+var ShopkeperRetun = MainMoney - TwoKgFruitsPrice;
+
+// console.log(ShopkeperRetun);
+
+
+// Task 2
+
+// all subject marks
+var Math = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physies = 35.45;
+var Bangla = 99.50;
+
+var TolalMarks = Math + Biology + Chemistry + Physies +
+Bangla;
+
+var subject = 5;
+
+var AvarageMarks = TolalMarks / subject;
+var desimal2 = AvarageMarks.toFixed(2);
+
+
+
+// console.log(TolalMarks);
+// console.log(desimal2);
+
+
+// Task 3
+
+var FristLine = "I am going to be"
+var ScoendLine = "an awesome web developer of the contain the string's"
+
+var TotalLine = FristLine + ' ' +ScoendLine;
+
+// console.log(TotalLine);
+
+
+// Task 4
+
+var ContainNumber = 119;
+var DividedNumber = 5;
+
+var divideNumber = ContainNumber % DividedNumber;
+
+// console.log(divideNumber);
+
